3d1db78f62d2e1400a23f72eabd9b0117ba14478
trentbaker <b.trent5@gmail.com>
2020-10-04 13:53:09 -0600

    paste in svelte app

---
+++ cookbook/src/Dial.svelte
@@ -0,0 +1,54 @@
+<script>
+    // constructor params
+    const dialSize = 500
+    const strokeWidthPercent = 0.3
+
+    const midpoint = (dialSize / 2)
+    const strokeWidth = midpoint * strokeWidthPercent
+
+    const radius = midpoint - (strokeWidth / 2) // strokes on path center
+    const circumference = 2 * Math.PI * radius
+
+    let progress = circumference
+    const incrementProgress = () => progress -= 150
+    const decrementProgress = () => progress += 75
+
+    console.log(`radius: ${radius}`)
+</script>
+
+<style>
+    :global(body) {
+        background-color: cornflowerblue;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        position: relative;
+    }
+</style>
+
+<svg width="{dialSize}" height="{dialSize}">
+
+    <circle r="{radius}"
+            cx="{midpoint}"
+            cy="{midpoint}"
+            fill="transparent"
+            stroke="white"
+            stroke-width="{strokeWidth}"
+            stroke-dasharray="{circumference}"
+            stroke-dashoffset="{progress}"
+            transform="rotate(90, {midpoint}, {midpoint})"
+    />
+
+    <text
+            x="{midpoint}"
+            y="{midpoint}"
+            text-anchor="middle"
+            dominant-baseline="middle"
+            font-size="{dialSize / 6}"
+            fill="white"
+    >{-(progress - circumference)}</text>
+</svg>
+
+<button on:click="{decrementProgress}">-</button>
+<button on:click="{incrementProgress}">+</button>
---
+++ cookbook/src/main.js
@@ -0,0 +1,8 @@
+import App from './App.svelte'
+import Dial from './Dial.svelte'
+
+const app = new Dial({
+	target: document.body
+});
+
+export default app;
